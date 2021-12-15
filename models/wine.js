e937838fd4fb9a9bcbce8e72151f6d83a8e2717d
BrandonCarver93 <brandon.t.carver93@gmail.com>
2021-12-15 17:14:02 -0500

    refactored wine and review schemas, added put request for ratings feature.

--- models/wine.js
+++ models/wine.js
@@ -1,21 +1,30 @@
 const mongoose = require('mongoose');
-const config = require('config');
+const Joi = require('joi');
 
 const reviewSchema = new mongoose.Schema({
     text: { type: String, required: true},
+    rating: {type: Number, default: 0 },
 })
 const wineSchema = new mongoose.Schema({
-    vineyard: {type: String, required: true},
-    varietal: {type: String, required: true},
-    pairing: {type: String, required: true},
-    description: {type: String, required: true},
-    price: {type: Number, required: true},
-    reviews: {type: [reviewSchema], default: []},
+    vineyard: { type: String, required: true },
+    varietal: { type: String, required: true },
+    pairing: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    reviews: { type: [reviewSchema], default: [] },
 });
+
+function validateRating(review) {
+    const schema = Joi.object({
+        rating: Joi.number().required()
+    });
+    return schema.validate(review)
+}
 
 const Review = mongoose.model('Review', reviewSchema);
 const Wine = mongoose.model('Wine', wineSchema);
-module.exports.Review = Review;
-module.exports.Wine = Wine;
-module.exports.wineSchema = wineSchema;
-module.exports.reviewSchema = reviewSchema;
+exports.Review = Review;
+exports.Wine = Wine;
+exports.wineSchema = wineSchema;
+exports.reviewSchema = reviewSchema;
+exports.validateRating = validateRating;
--- routes/wines.js
+++ routes/wines.js
@@ -1,42 +1,39 @@
-const { Wine, Review } = require("../models/wine");
+const { Wine, Review, validateRating } = require("../models/wine");
+const auth = require('../middleware/auth')
 const express = require('express');
 const router = express.Router();
 //* Get all wines
 router.get("/", async (req, res) => {
     try {
-      const wines = await Wine.find();
-      return res.send(wines);
+        const wines = await Wine.find();
+        return res.send(wines);
     } catch (ex) {
-      return res.status(500).send(`Internal Server Error: ${ex}`);
-    }
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+      }
   });
 
   //* Get wine by id
   router.get('/:wineId', async (req, res) => {
-      try {
-          const wine = await Wine.findById(req.params.wineId);
-          return res.send(wine);
+    try {
+        const wine = await Wine.findById(req.params.wineId);
+        return res.send(wine);
       } catch (ex) {
           return res.status(500).send(`Internal Server Error: ${ex}`);
-      }
+        }
   });
-//* Post Wines
+//* POST Wines
   router.post('/add', async (req, res) => {
     try {
-
         const wine = new Wine(req.body);
-
         await wine.save();
-
         return res.send(wine);
-
     } catch (ex) {
         return res.status(500).send(`Internal Server Error: ${ex}`);
-    }
+      }
 });
 
 //* POST reviews
-router.post('/:wineId/reviews', async (req, res) => {
+router.post('/:wineId/reviews', [auth], async (req, res) => {
   try{
       let wine = await Wine.findById(req.params.wineId);
       let review = new Review(req.body);
@@ -44,8 +41,32 @@ router.post('/:wineId/reviews', async (req, res) => {
       await wine.save();
       return res.send(wine);
   } catch (ex){
-    return res.status(500).send(`Internal Server Error: ${ex}`);
-  }
+      return res.status(500).send(`Internal Server Error: ${ex}`);
+    }
 });
   module.exports = router;
+
+  router.put('/:wineId', async (req, res) => {
+    try {
+        const {error} = validateRating(req.body);
+        if (error) return res.status(400).send(error);
+
+        const review = await Review.findByIdAndUpdate(req.params.wineId, 
+            {
+                rating: req.body.rating
+            },
+            { new: true }
+            );
+
+            if (!review)
+                return res.status(400).send(`The wine with id "${req.params.wineId}"
+                does not exist.`);
+
+            await review.save();
+
+            return res.send(review);
+    } catch (ex) {
+        return res.status(500).send(`Internal Server Error: ${ex}`);
+      }
+});
 
